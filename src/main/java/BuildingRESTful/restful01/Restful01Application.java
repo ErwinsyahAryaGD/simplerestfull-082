2aa90be4241d4c33687606e5fad80d3e2211cd11
USER <USER@DESKTOP-30P3A3H>
2022-12-05 10:50:03 +0700

    commit pertama

---
+++ src/main/java/BuildingRESTful/restful01/Restful01Application.java
@@ -0,0 +1,13 @@
+package BuildingRESTful.restful01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Restful01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Restful01Application.class, args);
+	}
+
+}
---
+++ src/test/java/BuildingRESTful/restful01/Restful01ApplicationTests.java
@@ -0,0 +1,13 @@
+package BuildingRESTful.restful01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Restful01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
